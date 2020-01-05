9fd549597e724dada31d56a0f256605cc4c241f9
Jed Thompson <jedster1111@hotmail.co.uk>
2020-01-05 18:32:55 +0000

    Iniitial backend neo4j data

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,7 @@ module.exports = {
   rules: {
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
     // e.g. "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/explicit-function-return-type": ["error", { allowExpressions: true }]
+    "@typescript-eslint/explicit-function-return-type": ["error", { allowExpressions: true }],
+    "@typescript-eslint/no-use-before-define": ["error", { "functions": false}]
   },
 };
--- workspaces/backend/src/server.ts
+++ workspaces/backend/src/server.ts
@@ -2,8 +2,17 @@ import Koa from "koa";
 import Router from "koa-router";
 import bodyParser from "koa-body";
 import CSRF from "koa-csrf";
+import neo4j from "neo4j-driver";
 
 import { logger } from "./middleware/logger";
+
+const driver = neo4j.driver(
+  "bolt://localhost:7687",
+  neo4j.auth.basic("neo4j", "password")
+);
+const session = driver.session();
+
+initData();
 
 const app = new Koa();
 
@@ -20,3 +29,31 @@ app.use(router.routes());
 app.listen(3000);
 
 console.log("Server running on port 3000");
+
+async function initData(): Promise<void> {
+  try {
+    console.log("Deleting all nodes");
+    await session.run(`MATCH (n) DETACH DELETE n`);
+
+    console.log("Creating initial data");
+    const result = await session.run(
+      `
+      CREATE (a:Athlete {name: $name})-[:TRAINS_AT]->(g:Gym {name: $gym})
+      RETURN a, g`,
+      {
+        name: "Jed",
+        gym: "Carlson Gracie London"
+      }
+    );
+
+    session.close();
+    const singleRecord = result.records[0];
+    const athlete = singleRecord.get("a");
+    const gym = singleRecord.get("g");
+    console.log(athlete.properties.name, "trains at", gym.properties.name);
+
+    driver.close();
+  } catch (e) {
+    console.log(e);
+  }
+}
